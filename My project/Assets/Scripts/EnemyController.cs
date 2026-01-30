a2b257500854b170d57e7505643da67f12a593a0
Thalruk <Bartek.lipinski.pl@gmail.com>
2026-01-30 20:10:22 +0100

    Enemy patrol etc

--- My project/Assets/Scripts/EnemyController.cs
+++ My project/Assets/Scripts/EnemyController.cs
@@ -1,29 +1,64 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class EnemyController : MonoBehaviour
 {
-    [SerializeField] Transform startPoint;
-    [SerializeField] Transform patrolPoint;
-    [SerializeField] bool movingTowardPatrol = true;
+    [SerializeField] List<PatrolPoint> patrolPoints;
+    [SerializeField] Transform patrolPointHolder;
+    int actualPatrolPoint = 0;
+
     Rigidbody2D rb2D;
+    [SerializeField] float speed;
+
+    [SerializeField] float timer = 0;
 
     private void Awake()
     {
+        patrolPoints.Clear();
         rb2D = GetComponent<Rigidbody2D>();
-        startPoint = transform;
+
+        patrolPoints.AddRange(patrolPointHolder.GetComponentsInChildren<PatrolPoint>());
+
+        if (patrolPoints.Count < 2)
+        {
+            Debug.LogError($"{name} doesnt have enough patrol points!");
+        }
     }
 
     private void Update()
     {
-        if (movingTowardPatrol)
+        if (Vector2.Distance(transform.position, patrolPoints[actualPatrolPoint].transform.position) < patrolPoints[actualPatrolPoint].checkRadius)
         {
-            rb2D.MovePosition(patrolPoint.position);
+            Debug.Log("close enough");
+            if (patrolPoints[actualPatrolPoint].waitTime != 0)
+            {
+                Debug.Log("have to wait");
+
+                if (timer >= patrolPoints[actualPatrolPoint].waitTime)
+                {
+                    actualPatrolPoint = (actualPatrolPoint + 1) % patrolPoints.Count;
+                    timer = 0;
+                    Debug.Log("finished waiting");
+
+                }
+                else
+                {
+                    timer += Time.deltaTime;
+                    Debug.Log("waiting");
+
+                }
+            }
+            else
+            {
+                actualPatrolPoint = (actualPatrolPoint + 1) % patrolPoints.Count;
+                Debug.Log("no wait");
+
+            }
+
         }
-        else
-        {
-            rb2D.MovePosition(startPoint.position);
-        }
+        Vector2 direction = (patrolPoints[actualPatrolPoint].transform.position - transform.position).normalized;
+        rb2D.velocity = direction * speed;
     }
 
 }
