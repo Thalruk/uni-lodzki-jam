5fcd1947a5b9e0449fa3a9bab7802d3e83fc53cf
Makar Oliinyk <deffffff4@gmail.com>
2026-01-31 14:21:33 +0100

    Spirit progress

--- My project/Assets/Scripts/Items/Masks/SpiritMask.cs
+++ My project/Assets/Scripts/Items/Masks/SpiritMask.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Rendering.PostProcessing;
 public class SpiritMask : ItemBaseClass
 {
+    GameObject camera;
     private void Start()
     {
         isPassive = true;
         isHasDurationTime = true;
         useDuration = 8f;
         cooldown = useDuration + cooldown;
+        camera = FindAnyObjectByType<Camera>().gameObject;
+        camera.GetComponent<PostProcessVolume>().enabled = false;
     }
-
+    protected override void Use()
+    {
+        camera.GetComponent<PostProcessVolume>().enabled = true;
+    }
+    protected override void Unequip()
+    {
+        camera.GetComponent<PostProcessVolume>().enabled = false;
+        base.Unequip();
+    }
 }
